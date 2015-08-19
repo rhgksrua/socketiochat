cdb887794a1648051f131a31415e3dbae3e25f09
Han Koh <rhgksrua@gmail.com>
2015-08-18 22:11:42 -0500

    added socket.io chat tutorial

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 #!/bin/env node
 //  OpenShift sample Node application
 var express = require('express');
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get('/', function(req, res) {
-    res.send('hi');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, ipaddress, function() {
-    console.log('ip:   ' + ipaddress);
-    console.log('port: ' + port);
+io.on('connection', function(socket) {
+    console.log('user connected');
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+
+    });
+   
+});
+
+http.listen(port, ipaddress, function() {
+    console.log('running... http://' + ipaddress + ":" + port);
 });
